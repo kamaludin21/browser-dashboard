2a4295cdc022c8db56d69d67ae7ee56cb674811d
Kamaludin <kamal.zyel@gmail.com>
2024-05-12 12:36:58 +0700

    custom font with variable font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-		"./index.html",
-		"./src/**/*.{js,ts,vue}"
-	],
+  content: ['./index.html', './src/**/*.{js,ts,vue}'],
   theme: {
-    extend: {},
+    fontFamily: {
+			sans: ['Satoshi-Variable', 'ui-sans-serif', 'system-ui']
+		},
+		extend: {}
   },
-  plugins: [],
-}
-
+  plugins: []
+};
