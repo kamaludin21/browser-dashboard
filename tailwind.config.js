e51cb8eee04f262bd7abcc867063e96089a4fa56
Kamaludin <kamal.zyel@gmail.com>
2024-05-12 12:54:01 +0700

    remove packages eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,vue}'],
   theme: {
-    fontFamily: {			
-			sans: ['Satoshi-Regular', 'ui-sans-serif', 'system-ui'],
-			light: ['Satoshi-Light', 'ui-sans-serif', 'system-ui'],
-			medium: ['Satoshi-Medium', 'ui-sans-serif', 'system-ui'],
-			bold: ['Satoshi-Bold', 'ui-sans-serif', 'system-ui'],
-			black: ['Satoshi-Black', 'ui-sans-serif', 'system-ui'],
-		},
-		extend: {}
+    fontFamily: {
+      sans: ['Satoshi-Regular', 'ui-sans-serif', 'system-ui'],
+      light: ['Satoshi-Light', 'ui-sans-serif', 'system-ui'],
+      medium: ['Satoshi-Medium', 'ui-sans-serif', 'system-ui'],
+      bold: ['Satoshi-Bold', 'ui-sans-serif', 'system-ui'],
+      black: ['Satoshi-Black', 'ui-sans-serif', 'system-ui']
+    },
+    extend: {}
   },
   plugins: []
-};
+}
